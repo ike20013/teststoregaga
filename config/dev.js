dc65e9e29207764d13ca133915426c854ae175d0
krinjmann <tavklandre@gmail.com>
2020-11-03 04:20:15 +0300

    init

--- config/dev.js
+++ config/dev.js
@@ -1,10 +1,10 @@
 module.exports = {
     mongoURI: '',
-    jwtSecret: '',
+    jwtSecret: '5B34C0874DC93FCC390E40FF617B4AB12271DDBA64C7F2A833FDB64BF78F7AA1',
     stripeSecret: 'pk_test_51HjCrpEbteUUuPkN7lgPzkmBTgOoxt2YLVfHHtSiYsW9iOqY1P0NoJ9s9KdHEBqYc19dVhjhOR7GBdZlKGHmabeb00GfEunSRk',
     endpointSecret: '',
-    s3SecretAccessKey: '',
-    s3AccessKeyId: '',
-    s3PublicBucket: '',
-    s3PrivateBucket: ''
+    s3SecretAccessKey: 'Cyk8RH1zxTXmZOcvzDsk9RCGHPTxj+9l9L8QO05W',
+    s3AccessKeyId: 'AKIAIEDTE3Z5KC2JBVUQ',
+    s3PublicBucket: 'arn:aws:s3:eu-central-1:450680851633:accesspoint/publicaccess',
+    s3PrivateBucket: 'arn:aws:s3:eu-central-1:450680851633:accesspoint/privateaccess'
 }
